27a02e9fdaf0decc9de49e5df80a1b3e90db9605
darkklord <ravi_yadav0502@yahoo.in>
2018-03-20 22:31:53 +0530

    Send fixture response as Json object

--- NodejsWebApp1/Service/FixtureService.js
+++ NodejsWebApp1/Service/FixtureService.js
@@ -2,7 +2,7 @@
 
 exports.getFixtures = function (req, res) {
     var fixtures = fixtureDb.getFixtures.getFixturesFromDb(function (data) {
-    res.write(data);
-    res.end();
+        var fixtureObj = JSON.parse(data);
+        res.send(fixtureObj);
     });
 }
